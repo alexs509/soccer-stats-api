e756a62a8b1d04107594f2a4bc9f8ab296b52467
Raphael <paiva.raphaelt@gmail.com>
2020-09-09 14:36:37 +0200

    add mongodb docker connexion

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 - virtualenv flask
 - (python -m pip install virtualenv)
 - flask/bin/pip install flask
+- pip install pymongo==3.4.0
+- Pour les systèmes UNIX installer -> https://gist.github.com/MarschHuynh/31c9375fc34a3e20c2d3b9eb8131d8f3
 
 ## RUN server (on cmd)
+- Pour mac -> source bin/activate
 - ./app.py
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import http.client
 import json
+from pymongo import MongoClient
+import jwt
 
+
+""" --- Connection at DB --- """
+
+client = MongoClient(
+    'mongodb://admin:admin@127.0.0.1:27017/foot?authSource=admin')
+db = client['foot']
+
+users = db.users
 
 """ --- Declare global variable --- """
 
@@ -10,11 +20,13 @@ app = Flask(__name__)
 API_FOOT_ENDPOINT = http.client.HTTPSConnection("v3.football.api-sports.io")
 headers = {
     'x-rapidapi-host': "v3.football.api-sports.io",
-    'x-rapidapi-key': "c8ba2608a4945269ae8378865c55cbb5"
-    }
+    'x-rapidapi-key': "dc8b40ef02db423d62cbd591ea1e6abc"
+}
 match = None
 
 """ --- Global functions --- """
+
+
 def allCountries():
     global match
     API_FOOT_ENDPOINT.request("GET", "/countries", headers=headers)
@@ -23,13 +35,16 @@ def allCountries():
     jsonData = json.loads((data.decode("utf-8")))
     return jsonData
 
+
 def nextMeet():
     global match
-    API_FOOT_ENDPOINT.request("GET", "/fixtures?league=61&season=2020", headers=headers)
+    API_FOOT_ENDPOINT.request(
+        "GET", "/fixtures?league=61&season=2020", headers=headers)
     res = API_FOOT_ENDPOINT.getresponse()
     data = res.read()
     jsonData = json.loads((data.decode("utf-8")))
     return jsonData
+
 
 def allLeagues():
     global match
@@ -39,6 +54,7 @@ def allLeagues():
     jsonData = json.loads((data.decode("utf-8")))
     return jsonData
 
+
 def teamsStats(x):
     global match
     API_FOOT_ENDPOINT.request("GET", "/teams?id="+x, headers=headers)
@@ -47,40 +63,82 @@ def teamsStats(x):
     jsonData = json.loads((data.decode("utf-8")))
     return jsonData
 
-def headToHead(x,y):
+
+def headToHead(x, y):
     global match
-    API_FOOT_ENDPOINT.request("GET", "/fixtures/headtohead?h2h="+x+"-"+y, headers=headers)
+    API_FOOT_ENDPOINT.request(
+        "GET", "/fixtures/headtohead?h2h="+x+"-"+y, headers=headers)
     res = API_FOOT_ENDPOINT.getresponse()
     data = res.read()
     jsonData = json.loads((data.decode("utf-8")))
     return jsonData
 
 
+def connection(username, password):
+    checkUser = users.find_one({'username': username})
+
+    if checkUser.get('password') == password:
+        valueReturn = jwt.encode(
+            {'some': 'payload'}, 'secret', algorithm='HS256')
+    else:
+        valueReturn = 'error'
+
+    return valueReturn
+
+
+def inscription(username, password):
+    if username != None or password != None:
+        users.insert_one(
+            {"username": username, "password": password})
+        return 'added'
+    else:
+        return 'error value'
+
+
 """ --- Routes --- """
+
+
 @app.route('/')
 def index():
     return "Server run !"
+
 
 @app.route('/api/v1.0/next-match', methods=['GET'])
 def next_meet():
     return nextMeet()
 
+
 @app.route('/api/v1.0/teams-stats', methods=['GET'])
 def teams_stats():
     return teamsStats("33")
+
 
 @app.route('/api/v1.0/countries', methods=['GET'])
 def get_countries():
     return allCountries()
 
+
 @app.route('/api/v1.0/headtohead', methods=['GET'])
 def get_headtohead():
-    return headToHead("33","34")
+    return headToHead("33", "34")
+
 
 @app.route('/api/v1.0/leagues', methods=['GET'])
 def get_leagues():
     return allLeagues()
-    
+
+
+@app.route('/api/v1.0/connection', methods=['POST'])
+def connectionRoute():
+    getForm = json.loads(request.data)
+    return connection(getForm.get('username'), getForm.get('password'))
+
+
+@app.route('/api/v1.0/inscription', methods=['POST'])
+def inscriptionRoute():
+    getForm = json.loads(request.data)
+    return inscription(getForm.get('username'), getForm.get('password'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
